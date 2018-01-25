eb80c33b592aeaa80957a759ef0a6f24700b34cf
merryHunter <chernuhaiv@gmail.com>
2018-01-25 11:52:56 +0100

    server has been deployed to Heroku

--- src/introsde/assignment3/soap/endpoint/PeoplePublisher.java
+++ src/introsde/assignment3/soap/endpoint/PeoplePublisher.java
@@ -9,22 +9,6 @@ import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
     public static void main(String[] args) throws UnknownHostException {
-        String PROTOCOL = "http://";
-        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
-        if (HOSTNAME.equals("127.0.0.1"))
-        {
-            HOSTNAME = "localhost";
-        }
-        String PORT = "6902";
-        String BASE_URL = "/as3/people";
-
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            PORT=String.valueOf(System.getenv("PORT"));
-        }
-        
-        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
-        System.out.println("Starting People Service...");
-        System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
-        Endpoint.publish(endpointUrl, new PeopleImpl());
+        Endpoint.publish("https://assignment3-chernukha.herokuapp.com/person", new PeopleImpl());
     }
 }
